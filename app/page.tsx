8f88a1cc037721ac7790fb19c51a4fc5e321ee74
ChiragG2101 <chiragg593@gmail.com>
2024-06-30 14:43:31 +0530

    starter setup with heroicons installation

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,12 @@
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-4 p-24 bg-purple-101 text-2xl">
-      Hello Digiwhistle
-      <Button className="drop-shadow-y-elevation-lg " size={"xl"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs" variant={"secondary"} size={"lg"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs" disabled variant={"secondary"} size={"lg"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs " disabled size={"default"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs" variant={"tertiary"} size={"sm"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs" disabled variant={"tertiary"} size={"sm"}>
-        Shadcn button
-      </Button>
-      <Button className="drop-shadow-y-elevation-xs" size={"xs"}>
-        Shadcn button
-      </Button>
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+      <Navbar />
+      <Hero />
     </main>
   );
 }
