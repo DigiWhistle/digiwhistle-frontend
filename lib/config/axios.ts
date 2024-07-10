3defa522c27f5c0e9700f92880cbcbfe09e78116
ChiragG2101 <chiragg593@gmail.com>
2024-07-10 12:55:10 +0530

    axios cors issue fix

--- lib/config/axios.ts
+++ lib/config/axios.ts
@@ -4,7 +4,7 @@ interface AxiosClientConfig {
   baseURL: string;
   headers: {
     "Content-Type": string;
-    Accept: string;
+    accept: string;
   };
   timeout: number;
   withCredentials: boolean;
@@ -14,10 +14,10 @@ const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Accept: "application/json",
+    accept: "application/json",
   },
   timeout: 20000,
-  withCredentials: true,
+  // withCredentials: true,
 } as AxiosClientConfig);
 
 export async function getRequest(URL: string): Promise<any> {
